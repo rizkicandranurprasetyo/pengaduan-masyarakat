bbf64bd3345529c285b77e6828e975d733ed1d9f
Rizki Candra Nur Prasetyo <81002706+rizkicandranurprasetyo@users.noreply.github.com>
2021-03-20 02:38:27 -0700

    Add files via upload

---
+++ admin/dashboard.php
@@ -0,0 +1,37 @@
+
+<h3 class="black-text">Dahsboard Pengaduan</h3>
+<h3 class="black-text">masyarakat Desa Cicadas</h3>
+
+	<div class="row">
+		<div class="col s4">
+		  <div class="card red">
+		    <div class="card-content white-text">
+			<?php 
+				$query = mysqli_query($koneksi,"SELECT * FROM pengaduan");
+				$jlmmember = mysqli_num_rows($query);
+				if($jlmmember<1){
+					$jlmmember=0;
+				}
+			 ?>
+		      <span class="card-title">Laporan Masuk<b class="right"><?php echo $jlmmember; ?></b></span>
+		      <p></p>
+		    </div>
+		  </div>
+		</div>	
+
+		<div class="col s4">
+		    <div class="card teal">
+		    <div class="card-content white-text">
+			<?php 
+				$query = mysqli_query($koneksi,"SELECT * FROM pengaduan WHERE status='selesai'");
+				$jlmmember = mysqli_num_rows($query);
+				if($jlmmember<1){
+					$jlmmember=0;
+				}
+			 ?>
+		      <span class="card-title">Laporan Selesai <b class="right"><?php echo $jlmmember; ?></b></span>
+		      <p></p>
+		    </div>
+		  </div>
+		</div>
+	</div>
---
+++ petugas/dashboard.php
@@ -0,0 +1,37 @@
+
+<h3 class="black-text">Dahsboard petugas </h3>
+<h3 class="black-text">Desa Cicadas</h3>
+
+	<div class="row">
+		<div class="col s4">
+		  <div class="card red">
+		    <div class="card-content white-text">
+			<?php 
+				$query = mysqli_query($koneksi,"SELECT * FROM pengaduan WHERE status='proses'");
+				$jlmmember = mysqli_num_rows($query);
+				if($jlmmember<1){
+					$jlmmember=0;
+				}
+			 ?>
+		      <span class="card-title">Laporan Status Proses<b class="right"><?php echo $jlmmember; ?></b></span>
+		      <p></p>
+		    </div>
+		  </div>
+		</div>	
+
+		<div class="col s4">
+		    <div class="card teal">
+		    <div class="card-content white-text">
+			<?php 
+				$query = mysqli_query($koneksi,"SELECT * FROM tanggapan WHERE id_petugas='".$_SESSION['data']['id_petugas']."'");
+				$jlmmember = mysqli_num_rows($query);
+				if($jlmmember<1){
+					$jlmmember=0;
+				}
+			 ?>
+		      <span class="card-title">Laporan Ditanggapi <b class="right"><?php echo $jlmmember; ?></b></span>
+		      <p></p>
+		    </div>
+		  </div>
+		</div>
+	</div>
